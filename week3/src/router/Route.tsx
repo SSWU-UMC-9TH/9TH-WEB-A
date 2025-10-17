c78928c03e4deacc6bae3ca249df3a09f3fa9b86
Gyeongmin <alsruddh0210@naver.com>
2025-10-17 15:53:09 +0900

    delete

--- week3/src/router/Route.tsx
+++
@@ -1,5 +0,0 @@
-import type { ComponentType } from 'react';
-
-type RouteProps = { path: string; component: ComponentType<any> };
-
-export const Route = ({ component: Component }: RouteProps) => <Component />;
--- week3/src/router/index.ts
+++
@@ -1,5 +0,0 @@
-export { Link } from './Link';
-export { Route } from './Route';
-export { Routes } from './Routes';
-export { useCurrentPath } from './hooks';
-export { getCurrentPath, navigateTo, PUSHSTATE_EVENT } from './util';
