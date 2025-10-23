0991de73c07ef613611864ed5f1fe9750c371e51
Maristella <maristella@Maristellas-MacBook-Pro.local>
2025-10-23 23:44:24 +0900

    feat: Week4_stella 미션2(#39)

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,75 @@
+import useForm from "../hooks/useForm.ts";
+import type { UserSigninInformation } from "../utils/validate";
+import { validateSignin } from "../utils/validate";
+
+
+const LoginPage = () => {
+    const { values, errors, touched, getInputProps } = 
+    useForm<UserSigninInformation>({
+        initialValue: {
+            email: "",
+            password: "",
+        },
+        validate: validateSignin,
+    });
+
+    const handleSubmit = () => {
+        console.log(values);
+    };
+
+
+    // 오류가 하나라도 있거나, 입력값이 비어있으면 버튼을 비활성화 
+    const isDisabled = 
+    Object.values(errors ?? {}).some((error) => (error ?? "").length > 0) || // 오류가 있으면 true
+    Object.values(values).some((value) => value === ""); // 입력값이 비어있으면 true
+
+  return (
+    <div className="flex flex-col items-center justify-center h-full gap-4">
+      <div className="flex flex-col gap-3">
+        <input
+            {...getInputProps('email')}
+            
+            name="email"
+          type="email"
+          placeholder="이메일"
+          className={`w-[300px] p-[10px] rounded-sm focus:border-[#807bff] border
+            ${
+              errors?.email && touched?.email
+                ? "border-red-500 bg-red-200"
+                : "border-[#ccc]"
+            }`}
+        />
+        {errors?.email && touched?.email && (
+          <div className="text-red-500 text-sm">{errors.email}</div>
+        )}
+
+        <input
+          {...getInputProps("password")}
+          type="password"
+          placeholder="비밀번호"
+          className={`w-[300px] p-[10px] rounded-sm focus:border-[#807bff] border
+            ${
+              errors?.password && touched?.password
+                ? "border-red-500 bg-red-200"
+                : "border-[#ccc]"
+            }`}
+        />
+        {errors?.password && touched?.password && (
+          <div className="text-red-500 text-sm">{errors.password}</div>
+        )}
+
+        <button 
+        type="button"
+        onClick={handleSubmit}
+        disabled={isDisabled}
+        className="w-full bg-blue-600 text-white py-3 rounded-md text-lg font-medium hover:bg-blue-700 transition-colors disabled:bg-gray-300 disabled:cursor-not-allowed"
+        >
+            로그인
+        </button>
+
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
